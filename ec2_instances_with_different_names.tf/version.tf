48a61ad3bb6c95d3dd006b1254bbbbc29b106c9c
Manoj G <102495616+manojmanu276@users.noreply.github.com>
2023-09-26 12:46:31 +0530

    Delete ec2_instances_with_different_names.tf directory

--- ec2_instances_with_different_names.tf/version.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-   required_version=">=1.2.0"     #here mention the required version of terraform you want to use to provision the aws infrastructure
-   required_providers {
-     aws = {
-       source="hashicorp/aws"
-       version="~>4.16"           #here mention the required version of aws
-     }
-   }
-}
