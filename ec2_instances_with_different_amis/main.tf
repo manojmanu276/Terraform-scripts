6fb2b972f210e1dcec4f46fcae314fa74abeed63
Manoj G <102495616+manojmanu276@users.noreply.github.com>
2023-09-27 17:10:42 +0530

    creating multiple ec2 instances with different amis using single resource block

--- ec2_instances_with_different_amis/main.tf
+++ ec2_instances_with_different_amis/main.tf
@@ -2,12 +2,12 @@ locals {
   instance_names={"instance1":"ami-067c21fb1979f0b27","instance2":"ami-06384cb50db0a13f5","instance3":"ami-08abb3eeacc61972d"}
 }
 resource "aws_instance" "ec2_test_instances" {
-  key_name = var.key_name  
-  instance_type = var.instance_type
-  for_each = local.instance_names #takes instance names for mapping locals variable declared above
-  ami = each.value   #takes ami-ids from values of mapping locals variable 
+  key_name = var.key_name             #takes key_name from variable declared in variables.tf file
+  instance_type = var.instance_type   #gets instance_type from variable declared in variables.tf file
+  for_each = local.instance_names     #takes instance names for mapping locals variable declared above
+  ami = each.value                    #takes ami-ids from values of mapping locals variable 
   tags = {
-    Name= each.key  #takes instance names form from keys of mapping locals variable
+    Name= each.key                    #takes instance names form from keys of mapping locals variable
   }
 }
 
