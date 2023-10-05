1ba748809294d24d2d9858a30a8f384ea16c96e9
Manoj G <102495616+manojmanu276@users.noreply.github.com>
2023-10-05 10:41:19 +0530

    Update var.tf

--- ec2_instances_with_different_amis/var.tf
+++ ec2_instances_with_different_amis/var.tf
@@ -1,10 +1,10 @@
 variable "instance_type" {
   default = "t2.micro"
   description = "this is instance that belongs to t2 family of instances and provides 1 virtual cpu, 1GiB memory hence the name t2.micro"
-  type="string"
+  type=string
 }
 variable "key_name" {
   default = "your-key-name(pem-file-name)"
   description = "Privacy Enhanced Mail (PEM) files are a type of Public Key Infrastructure (PKI) file used for keys and certificates"
-  type="string"
+  type=string
 }
