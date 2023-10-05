8cb74dcfa3deb133780c077f2ac4a293d8cb6ae4
Manoj G <102495616+manojmanu276@users.noreply.github.com>
2023-10-05 10:30:33 +0530

    Update var.tf

--- s3-bucket/var.tf
+++ s3-bucket/var.tf
@@ -13,3 +13,22 @@ variable "bucket_name" {
   description = "name of the s3 bucket i want to provision"
   type = "string"
 }
+variable "target" {
+  default = "logs-bucket-tf"
+  description = "name of the bucket where access logs will be stored"
+  type = "string"
+}
+variable "sse" {
+  default = "AES256"
+  description = "advanced encryption standard encrypts data at rest in s3"    # server side encryption algorithm, can also be "aws:kms" for AWS Key Management service                                            
+  type = "string"                                                       # Advanced Encryption Standard(AES) is a symmetric block cipher used to protect classified data      
+}  
+variable "environment" {
+  default = "DEV"
+  description = "environment in which you want s3 to be created"
+  type = "string"
+}
+variable "class" {
+  default= "GLACIER"
+  description = "storage class to which you have to move s3 objects after certain number of days"
+}
